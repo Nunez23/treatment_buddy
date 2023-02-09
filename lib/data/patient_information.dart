b51aaa44bcfcd2b01a6b28058fb52165f4df074c
Nunez23 <aluochvelma12@gmail.com>
2023-02-09 15:41:05 +0300

    first commit

---
+++ lib/data/patient_information.dart
@@ -0,0 +1,28 @@
+class PatientInformation {
+  String name;
+  String age;
+  String gender;
+  String phoneNumber;
+  String address;
+  String weight;
+  String height;
+  String bmi;
+  String diagnosis;
+  String medicine;
+  String treatmentBuddy;
+
+//constructor with optional named parameter
+  PatientInformation(
+    this.name,
+    this.age,
+    this.gender,
+    this.phoneNumber,
+    this.address,
+    this.weight,
+    this.height,
+    this.bmi,
+    this.diagnosis,
+    this.medicine,
+    this.treatmentBuddy,
+  );
+}
---
+++ lib/ui/patient_information/patient_information.page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+
+class PatientListPage extends StatelessWidget {
+  const PatientListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Patient information')),
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Text(faker.person.firstName(),
+                semanticsLabel: faker.person.lastName()),
+          );
+        },
+      ),
+    );
+  }
+}
